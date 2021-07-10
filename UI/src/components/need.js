e468ea7b6090089e49b7dde41e4f048f4e87d27a
abhishekmaletha <abhishek.maletha2000@gmail.com>
2021-07-11 02:22:34 +0530

    circle finishing remains

--- UI/src/components/need.js
+++ UI/src/components/need.js
@@ -1,27 +1,24 @@
 
-var stx=0;
-var sty=0;
+var stx = 0;
+var sty = 0;
 var group = new Group();
 function onMouseDown(event) {
-   
-   stx=event.point.x;
-   sty=event.point.y;
-   console.log(stx," ",sty);
-   
+
+    stx = event.point.x;
+    sty = event.point.y;
+    // console.log(stx," ",sty);
+
 }
 
 function onMouseDrag(event) {
     group.removeChildren()
-    var rectangle = new Rectangle(new Point(stx, sty), new Point(event.point));
-    var myPath = new Path.Rectangle(rectangle);
-    myPath.fillColor = '#e9e9ff';
-    group.addChild(myPath);
+    var myCircle = new Path.Circle(new Point(stx, sty), (stx - event.point.x));
+    myCircle.strokeColor = 'black';
+    group.addChild(myCircle);
     // myPath.selected = true;
 }
 function onMouseUp(event) {
 
-    var rectangle = new Rectangle(new Point(stx, sty), new Point(event.point));
-    var path = new Path.Rectangle(rectangle);
-    path.fillColor = '#e9e9ff';
-    path.selected = true;
+    var myCircle = new Path.Circle(new Point(stx, sty), (stx - event.point.x));
+    myCircle.strokeColor = 'black';
 }
